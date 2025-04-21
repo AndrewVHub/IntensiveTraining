47d68b026e3fbb9c261c19b7036451efb5bb6736
Andrey Vykhodtsev <os4.official@gmail.com>
2025-04-21 14:19:30 +0700

    init + add libs

---
+++ app/build.gradle.kts
@@ -0,0 +1,95 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "ru.andrewvhub.intensivetraining"
+    compileSdk = 35
+
+    viewBinding.enable = true
+
+    defaultConfig {
+        applicationId = "ru.andrewvhub.intensivetraining"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+
+        debug {
+            isMinifyEnabled = false
+            isDebuggable = true
+            resValue("string", "app_name", "Intensive Training Debug")
+        }
+
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    //Lifecycle
+    implementation(libs.androidx.lifecycle.runtime)
+    implementation(libs.androidx.lifecycle.common)
+    implementation(libs.androidx.lifecycle.viewmodel)
+    implementation(libs.androidx.lifecycle.livedata)
+
+    //Koin
+    implementation(libs.koin)
+
+    //Navigation Component
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.navigation.ui)
+
+    //Glide
+    implementation(libs.glide)
+
+    //Timber
+    implementation(libs.timber)
+
+    //RecyclerView
+    implementation(libs.androidx.recyclerView)
+
+    //Lottie Animation
+    implementation(libs.lottie.animation)
+
+    //Charts
+    implementation(libs.mp.android.chart)
+
+    //Gson
+    implementation(libs.gson)
+
+    //Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.gson.converter)
+
+    //OkHttp3
+    implementation(libs.logging.interceptor)
+}
